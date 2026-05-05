2ecd8a622ba1bf00a54efd3ac5c1eff164ebdca0
cvsz <seaza@msn.com>
2026-05-05 08:49:33 +0700

    Update init.sh

--- infra/observability/elasticsearch/init.sh
+++ infra/observability/elasticsearch/init.sh
@@ -1,11 +1,28 @@
-# path: infra/observability/elasticsearch/init.sh
+#!/bin/sh
+set -e
 
-curl -u elastic:$ELASTIC_PASSWORD -k \
-  -X PUT "https://localhost:9200/_index_template/logs-template" \
+ELASTIC_URL=${ELASTIC_URL:-https://elasticsearch:9200}
+USER=elastic
+PASS=${ELASTIC_PASSWORD}
+
+# Index template
+curl -u $USER:$PASS -k -X PUT "$ELASTIC_URL/_index_template/logs-template" \
   -H "Content-Type: application/json" \
   -d @index-template.json
 
-curl -u elastic:$ELASTIC_PASSWORD -k \
-  -X PUT "https://localhost:9200/_ilm/policy/logs-policy" \
+# ILM policy
+curl -u $USER:$PASS -k -X PUT "$ELASTIC_URL/_ilm/policy/logs-policy" \
   -H "Content-Type: application/json" \
   -d @ilm-policy.json
+
+# Bootstrap index
+curl -u $USER:$PASS -k -X PUT "$ELASTIC_URL/logs-000001" \
+  -H "Content-Type: application/json" \
+  -d '{
+    "aliases": {
+      "logs": { "is_write_index": true }
+    },
+    "settings": {
+      "index.lifecycle.name": "logs-policy"
+    }
+  }'
